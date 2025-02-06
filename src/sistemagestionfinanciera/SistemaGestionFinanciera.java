397a4816fc256d8e1eb000379c999187db9e58e8
emaenriquez <emaenriquez1985@gmail.com>
2025-02-05 21:57:48 -0300

    Financial Management System

---
+++ src/sistemagestionfinanciera/SistemaGestionFinanciera.java
@@ -0,0 +1,10 @@
+
+package sistemagestionfinanciera;
+
+public class SistemaGestionFinanciera {
+
+    public static void main(String[] args) {
+        System.out.print("hola mundo");
+    }
+    
+}
